81111de7ab8fb122f1b75c3f5f4cbf9aeca546f6
Neil Thomas <thomas.a.neil@gmail.com>
2018-02-21 17:56:44 -0800

    Fix path to test file to work with pytest from root

--- test/test_sfs.py
+++ test/test_sfs.py
@@ -1,30 +1,29 @@
-from momi import Demography
-import pytest
-from test_demography import demo
 import cPickle as pickle
-import numpy as np
 import itertools
 
-results_file = 'test_sfs.pickle'
+import pytest
+import numpy as np
 
-try:
-    with file(results_file,'r') as f:
-        results = pickle.load(f)
-except:
-    pass
+from test_demography import demo
+
+results_file = 'test/test_sfs.pickle'
+
+with open(results_file, 'r') as f:
+    results = pickle.load(f)
+
 
 @pytest.mark.parametrize("use_chen_eqs", (True,False))
 def test_compute_sfs(demo, use_chen_eqs):
     assert np.allclose(demo.compute_sfs(results['derived_counts'], use_chen_eqs=use_chen_eqs),
                        results['sfs_vals'])
 
-@pytest.mark.parametrize("use_chen_eqs", (True,False))    
+@pytest.mark.parametrize("use_chen_eqs", (True,False))
 def test_old_sfs(demo, use_chen_eqs):
     for i,config in enumerate(results['derived_counts']):
         config = {l : {'derived' : d,
                        'ancestral' : demo.n_lineages_subtended_by[l] - d} for l,d in config.iteritems()}
         assert np.isclose(demo.sfs(config, use_chen_eqs=use_chen_eqs), results['sfs_vals'][i])
-       
+
 if __name__=="__main__":
     derived_counts = [{'a': i, 'b': j}
                       for i,j in itertools.product(range(11), range(9))
@@ -35,4 +34,4 @@ if __name__=="__main__":
         pickle.dump({'derived_counts': derived_counts,
                      'sfs_vals' : sfs_vals},
                     f)
-    
+
