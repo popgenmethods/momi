c4cdc929f547b9dfbb419c46b7f833d1d1829a6a
Jack Kamm <jackkamm@gmail.com>
2018-02-26 17:18:21 +0000

    stuff for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
+from setuptools import setup, Extension
 import numpy
 
 try:
@@ -31,7 +29,7 @@ else:
 # see https://stackoverflow.com/a/4515279/3718509 and https://packaging.python.org/tutorials/distributing-packages/ for uploading to pypi
 
 setup(name='momi',
-      version='1.2.5',
+      version='1.2.7',
       description='Compute the site frequency spectrum (SFS) of population genetics',
       author='Jack Kamm, Jonathan Terhorst, Yun S. Song',
       author_email='jkamm@stat.berkeley.edu, terhorst@stat.berkeley.edu, yss@eecs.berkeley.edu',
